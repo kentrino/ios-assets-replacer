8a49e9cb8932c1947aae824f980cc9cc4ed4bf81
kentrino <kentrino@h-kento.jp>
2016-08-19 17:33:25 +0900

    Underscorization worked!

--- underscorize.rb
+++ underscorize.rb
@@ -5,7 +5,7 @@ require_relative 'rswift_processor'
 
 class Underscorize
   PROJECT_PATH = '/Users/kento/ios/HatalikeSwift/HatalikeSwift/'
-  NOOP = true
+  NOOP = false
 
   class UnderscorizeRenamer 
     def self.rename(original_name)
@@ -16,9 +16,9 @@ class Underscorize
   class UnderscorizeXmlProcessor < XmlProcessor
     def rename(original_name)
       new_name = UnderscorizeRenamer.rename(original_name)
-      p original_name
-      p source_imageset_path = ProjectUtils.imageset_path_from_resource_name(PROJECT_PATH, original_name)
+      source_imageset_path = ProjectUtils.imageset_path_from_resource_name(PROJECT_PATH, original_name)
       distination_resource_name_with_path = ImagesetCopier.default_distination_path_from_resource_name(source_imageset_path, new_name)
+
       @rename_agenda[original_name] = distination_resource_name_with_path
 
       new_name
@@ -48,33 +48,25 @@ class Underscorize
     end
   end
 
-
   def run
-    # p ImagesetCopier.default_distination_path_from_resource_name('/Users/kento/ios/HatalikeSwift/HatalikeSwift/Assets.xcassets/icon/icon_arrow.imageset', 'IconArrow')
-    rename_agenda = {} 
+    rename_agenda = {}
+
     ProjectUtils.list_all_storyboard(PROJECT_PATH).each do |storyboard_path|
       xml_processor = UnderscorizeXmlProcessor.new(storyboard_path)
-      xml_processor.run(noop: true)
+      xml_processor.run(noop: NOOP)
       rename_agenda.merge!(xml_processor.rename_agenda)
-      # 本番はここでImageSetの実行計画を作る
-      # 実行計画は元の名前：後の名前形式のハッシュ
     end
 
     ProjectUtils.list_all_xib(PROJECT_PATH).each do |xib_path|
       xml_processor = UnderscorizeXmlProcessor.new(xib_path)
       xml_processor.run(noop: NOOP)
       rename_agenda.merge!(xml_processor.rename_agenda)
-      # 本番はここでImageSetの実行計画をつくる
-      # 実行計画は元の名前：後の名前形式のハッシュ
     end
 
-    # 本番はセルとコントローラーのクラス名から似たような処理を行う
     ProjectUtils.list_all_swift(PROJECT_PATH).each do |swift_path|
       rswift_processor = UnderscorizeRswiftProcessor.new(swift_path)
       rswift_processor.run(noop: NOOP)
       rename_agenda.merge!(rswift_processor.rename_agenda)
-      # 本番はここでImageSetの実行計画をつくる
-      # 実行計画は元の名前：後の名前形式のハッシュ
     end
 
     rename_agenda.each do |original_name, new_name_with_path|
@@ -82,20 +74,7 @@ class Underscorize
       imageset_copier = ImagesetCopier.new(source_imageset_path, new_name_with_path)
       imageset_copier.move(noop: NOOP)
     end
-=begin
-    resource_names.uniq.each do |resource_name|
-      # Imagesetを書き換える
-      source_imageset_path = ProjectUtils.imageset_path_from_resource_name(@project_path, resource_name)
-      distination_resource_name_with_path = ImagesetCopier.default_distination_path_from_resource_name(source_imageset_path, UnderscorizeRenamer.rename(resource_name))
-      imageset_copier = ImagesetCopier.new(source_imageset_path, distination_resource_name_with_path)
-      # imageset_copier.move
-    end
-=end
   end
-
-  private
 end
 
-# UnderscorizeProcessor.new('/Users/kento/ios/HatalikeSwift/HatalikeSwift/View/SearchByJob/SearchByJobStoryboard.storyboard').run
 Underscorize.new.run
-
